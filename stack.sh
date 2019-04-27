e8e2a82598da7a7bac1e431217f5e4b4ba583c20
shokohsc <shokohsc@gmail.com>
2019-04-27 21:28:12 +0200

    first commit

---
+++ stack.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if [[ -z $1 ]]
+then
+    echo '#### ====>  starting basic stack containers: php-fpm nginx varnish <==== ####' && \
+    docker-compose up -d --remove-orphans php-fpm nginx varnish
+
+elif [[ $1 = 'pull' ]]
+then
+    echo '#### ====>  pulling container images <==== ####' && \
+    docker-compose pull
+
+elif [[ $1 = 'build' ]]
+then
+    echo '#### ====>  building container images <==== ####' && \
+    docker-compose build
+
+elif [[ $1 = 'rm' ]]
+then
+    echo '#### ====>  removing containers <==== ####' && \
+    docker-compose rm -sfv && \
+    docker network prune -f
+
+elif [[ $1 = 'stop' ]]
+then
+    echo '#### ====>  stopping containers <==== ####' && \
+    docker-compose stop
+
+else
+    echo "How to use ?
+    ./stack.sh
+    ./stack.sh pull
+    ./stack.sh build
+    ./stack.sh stop
+    ./stack.sh rm"
+fi
