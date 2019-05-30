b9512c9b04664ad3945cd2e01a460f9a7f01400a
shokohsc <shokohsc@gmail.com>
2019-05-31 00:24:48 +0200

    added python/gunicorn

--- stack.sh
+++ stack.sh
@@ -1,37 +1,51 @@
 #!/bin/bash
 
-if [[ -z $1 ]]
+if [[ $1 = 'symfony' ]]
 then
     echo '#### ====>  starting basic stack containers: php-fpm nginx varnish elasticsearch logstash kibana sqlite <==== ####' && \
-    docker-compose up -d --remove-orphans php-fpm nginx varnish elasticsearch logstash kibana sqlite
+    docker-compose -f docker-compose.php.yml up -d --remove-orphans php-fpm nginx varnish elasticsearch logstash kibana sqlite
+
+elif [[ $1 = 'flask' ]]
+then
+    echo '#### ====>  starting basic stack containers: gunicorn <==== ####' && \
+    docker-compose -f docker-compose.python.yml up -d --remove-orphans gunicorn
 
 elif [[ $1 = 'pull' ]]
 then
     echo '#### ====>  pulling container images <==== ####' && \
-    docker-compose pull
+    docker-compose -f docker-compose.php.yml pull && \
+    docker-compose -f docker-compose.python.yml pull
 
-elif [[ $1 = 'build' ]]
+elif [[ $1 = 'build' && $2 = 'symfony' ]]
 then
     echo '#### ====>  building container images <==== ####' && \
-    docker-compose build
+    docker-compose -f docker-compose.php.yml build
+
+elif [[ $1 = 'build' && $2 = 'flask' ]]
+then
+    echo '#### ====>  building container images <==== ####' && \
+    docker-compose -f docker-compose.python.yml build
 
 elif [[ $1 = 'rm' ]]
 then
     echo '#### ====>  removing containers <==== ####' && \
-    docker-compose rm -sfv && \
+    docker-compose -f docker-compose.php.yml rm -sfv && \
+    docker-compose -f docker-compose.python.yml rm -sfv && \
     docker network prune -f && \
     docker volume prune -f
 
 elif [[ $1 = 'stop' ]]
 then
     echo '#### ====>  stopping containers <==== ####' && \
-    docker-compose stop
+    docker-compose -f docker-compose.php.yml stop && \
+    docker-compose -f docker-compose.python.yml stop
 
 else
     echo "How to use ?
-    ./stack.sh
+    ./stack.sh symfony
+    ./stack.sh flask
     ./stack.sh pull
-    ./stack.sh build
+    ./stack.sh build symfony|flask
     ./stack.sh stop
     ./stack.sh rm"
 fi
